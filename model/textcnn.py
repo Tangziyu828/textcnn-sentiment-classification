49698444a8b79019402a279c73e3d50f16c329e6
Tangziyu828 <tangziyu2004828@qq.com>
2025-06-20 21:20:24 +0800

    Update textcnn.py

--- model/textcnn.py
+++ model/textcnn.py
@@ -1,1 +1,24 @@
-评论数据集（CSV格式）
+# model/textcnn.py
+
+import torch
+import torch.nn as nn
+
+class TextCNN(nn.Module):
+    def __init__(self, vocab_size, embed_dim, num_classes):
+        super(TextCNN, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embed_dim, padding_idx=0)
+        self.convs = nn.ModuleList([
+            nn.Conv2d(1, 100, (k, embed_dim)) for k in [3, 4, 5]
+        ])
+        self.dropout = nn.Dropout(0.5)
+        self.fc = nn.Linear(100 * 3, num_classes)
+
+    def forward(self, x):
+        x = self.embedding(x)         # (B, L, D)
+        x = x.unsqueeze(1)            # (B, 1, L, D)
+        x = [torch.relu(conv(x)).squeeze(3) for conv in self.convs]
+        x = [torch.max_pool1d(i, i.size(2)).squeeze(2) for i in x]
+        x = torch.cat(x, 1)
+        x = self.dropout(x)
+        return self.fc(x)
+
